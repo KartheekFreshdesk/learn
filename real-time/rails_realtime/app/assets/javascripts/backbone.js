d26b86ea91d3e417c33c2e07c0ce153e4313e5ac
KartheekFreshdesk <kartheekb@freshdesk.com>
2016-11-30 15:40:18 +0530

    sample realtime-rails for understanding done.

---
+++ real-time/rails_realtime/app/assets/javascripts/backbone.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require bootstrap
+//= require underscore
+//= require backbone
+//= require socket.io
+//= require app
---
+++ real-time/rails_realtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRedisSocket::Application.config.secret_token = '2c42eccaf22de0b312681710da767d3c3281c38e42f78e39651022b387d8187047244660562c289fb2d6874ca93c8639ef9b47110a429acc86195e08533b017d'
